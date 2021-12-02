bee1c61b85cd0ff64d99a82e8bb3dc8a30c64299
adamhacker90 <85889806+adamhacker90@users.noreply.github.com>
2021-12-02 10:18:47 -0500

    Add files via upload

---
+++ config_check/run.bat
@@ -0,0 +1,4 @@
+web_check.exe "C:\Users\ahacker\Desktop\JPMC_Config\MS71719\Config\web.ini"
+recorder_check.exe "C:\Users\ahacker\Desktop\JPMC_Config\MS71719\Config\recorder.ini"
+nas.exe "C:\Users\ahacker\Desktop\JPMC_Config\REC\Config\nas.ini"
+pp.exe "C:\Users\ahacker\Desktop\JPMC_Config\REC\Config\pp\pp.ini"
